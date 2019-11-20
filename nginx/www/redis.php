87d4acf1f0e1eb2047ab838d6c5e44e394a7cf4b
lbdong <lbdong2013@gmail.com>
2019-11-20 17:47:11 +0800

    迁移log目录位置

--- nginx/www/redis.php
+++ nginx/www/redis.php
@@ -2,5 +2,5 @@
 
 $redis = new Redis();
 $redis->connect('redis',6379);
-$redis->set(isset($_GET['key']) ? $_GET['key'] : 'test','hello world!');
-echo $redis->get('test');
+$redis->set(isset($_GET['key']) ? $_GET['key'] : 'test',isset($_GET['value']) ? $_GET['value'] : 'hello world!');
+echo $redis->get($_GET['key']);
