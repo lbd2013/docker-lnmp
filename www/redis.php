b23e03882f760cffece79d9d7a18069bb87fc471
lbdong <lbdong2013@gmail.com>
2019-11-21 17:13:48 +0800

    redis测试

--- www/redis.php
+++ www/redis.php
@@ -1,6 +1,7 @@
 <?php
-
 $redis = new Redis();
 $redis->connect('redis',6379);
-$redis->set(isset($_GET['key']) ? $_GET['key'] : 'test',isset($_GET['value']) ? $_GET['value'] : 'hello world!');
-echo $redis->get($_GET['key']);
+$key = isset($_GET['key']) ? $_GET['key'] : 'test';
+$value = isset($_GET['value']) ? $_GET['value'] : 'hello world!';
+$redis->set($key, $value);
+echo $redis->get($key);
