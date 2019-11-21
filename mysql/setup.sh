0f6ad0e956e9d5e89b7caff3f9a169aca93fa0ba
lbdong <lbdong2013@gmail.com>
2019-11-21 09:44:09 +0800

    mysql 初始化脚本

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -e
-mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
-source $WORK_PATH/$FILE_0;
-EOF
+
+#mysql状态，用于调试
+echo `service mysql status`
+
+echo '1.start mysql'
+mysql -uroot -p$MYSQL_ROOT_PASSWORD
+
+sleep 3
+echo `service mysql status`
+
+echo '2.修改类型'
+mysql < $WORK_PATH/$FILE_0;
+
+sleep 3
+echo `service mysql status`
+
+tail -f /dev/null
