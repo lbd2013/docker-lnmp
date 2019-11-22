bad203d898af3f938c1340f549060ccb287d8a07
lbdong <lbdong2013@gmail.com>
2019-11-22 17:13:54 +0800

    增加 memcache测试

--- www/memcached.php
+++ www/memcached.php
@@ -1,1 +1,10 @@
 <?php
+error_reporting(E_ALL);
+header('Content-type:text/plain');
+$mc = new Memcached();
+$mc->addServer("memcached", 11211);
+
+$flag = $mc->add('name','droidos');
+echo 1;
+echo ($flag)?'y':'n';
+echo $mc->getResultCode();
