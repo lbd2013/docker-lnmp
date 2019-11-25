c1ad0904c0deaead4687b3f8085ccabc7d3621bc
lbdong <lbdong2013@gmail.com>
2019-11-25 15:22:18 +0800

    修改mongo网络，增加mongo测试文件

--- www/mongo.php
+++ www/mongo.php
@@ -1,9 +1,26 @@
 <?php
-$m = new MongoClient();
-$db = $m->admin;
-$collection = $db->system->users;
+$filter = ['x' => ['$gt' => 1]];
+$options = [
+    'projection' => ['_id' => 0],
+    'sort' => ['x' => -1],
+];
+$manager = new MongoDB\Driver\Manager("mongodb://root:1234567@mongod/admin:27017?authSource=admin");
 
-$cursor = $collection->find();
-foreach ($cursor as $document) {
-    var_dump($document);
+if (!isset($_GET['type']) || $_GET['type'] == 'write') {
+    // 插入数据
+    $bulk = new MongoDB\Driver\BulkWrite;
+    $bulk->insert(['x' => 1, 'name' => '菜鸟教程', 'url' => 'http://www.runoob.com']);
+    $bulk->insert(['x' => 2, 'name' => 'Google', 'url' => 'http://www.google.com']);
+    $bulk->insert(['x' => 3, 'name' => 'taobao', 'url' => 'http://www.taobao.com']);
+    $manager->executeBulkWrite('admin.sites', $bulk);
+    echo "写入成功";
+} else {
+    // 查询数据
+    $query = new MongoDB\Driver\Query($filter, $options);
+    $cursor = $manager->executeQuery('admin.sites', $query);
+
+    foreach ($cursor as $document) {
+        print_r($document);
+    }
+    echo "读取成功";
 }
