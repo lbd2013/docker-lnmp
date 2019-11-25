30122fc1d9d95881fe8b2555de6bd3abe1cf8256
lbdong <lbdong2013@gmail.com>
2019-11-25 15:25:24 +0800

    修改mongo网络，增加mongo测试文件

--- www/mongo.php
+++ www/mongo.php
@@ -9,11 +9,11 @@ $manager = new MongoDB\Driver\Manager("mongodb://root:1234567@mongod/admin:27017
 if (!isset($_GET['type']) || $_GET['type'] == 'write') {
     // 插入数据
     $bulk = new MongoDB\Driver\BulkWrite;
-    $bulk->insert(['x' => 1, 'name' => '菜鸟教程', 'url' => 'http://www.runoob.com']);
+    $bulk->insert(['x' => 1, 'name' => 'test', 'url' => 'http://www.runoob.com']);
     $bulk->insert(['x' => 2, 'name' => 'Google', 'url' => 'http://www.google.com']);
     $bulk->insert(['x' => 3, 'name' => 'taobao', 'url' => 'http://www.taobao.com']);
     $manager->executeBulkWrite('admin.sites', $bulk);
-    echo "写入成功";
+    var_dump( "写入成功");
 } else {
     // 查询数据
     $query = new MongoDB\Driver\Query($filter, $options);
@@ -22,5 +22,5 @@ if (!isset($_GET['type']) || $_GET['type'] == 'write') {
     foreach ($cursor as $document) {
         print_r($document);
     }
-    echo "读取成功";
+    var_dump( "读取成功");
 }
