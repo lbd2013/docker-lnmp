01fbbab8a37ad650211cf66ff94f1aef328f0c73
lbdong <lbdong2013@gmail.com>
2019-11-28 16:01:06 +0800

    beanstalk 测试

--- www/beanstalkd.php
+++ www/beanstalkd.php
@@ -1,7 +1,7 @@
 <?php
-//执行以下语句，安装beanstalk php库，github地址（https://github.com/pheanstalk/pheanstalk）
-//docker exec php composer require pda/pheanstalk
-//docker exec php composer update -vvv
+echo "执行以下语句，安装beanstalk php库，（库 github地址 ：https://github.com/pheanstalk/pheanstalk）</br>";
+echo "docker exec php composer require pda/pheanstalk </br>";
+echo "//docker exec php composer update -vvv </br>";
 
 
 // Hopefully you're using Composer autoloading.
@@ -9,21 +9,24 @@ include_once "vendor/autoload.php";
 use Pheanstalk\Pheanstalk;
 
 // Create using autodetection of socket implementation
-$pheanstalk = Pheanstalk::create('127.0.0.1');
+$pheanstalk = Pheanstalk::create('beanstalkd');
 
 // ----------------------------------------
 // producer (queues jobs)
 
+$database = isset($_GET['database']) ? $_GET['database'] : 'default';
+$value    = isset($_GET['value']) ? $_GET['value'] : 'test';
+
 $pheanstalk
-    ->useTube('testtube')
-    ->put("job payload goes here\n");
+    ->useTube($database)
+    ->put($value);
 
 // ----------------------------------------
 // worker (performs jobs)
 
 $job = $pheanstalk
-    ->watch('testtube')
-    ->ignore('default')
+    ->watch($database)
+    ->ignore($value)
     ->reserve();
 
 echo $job->getData();
