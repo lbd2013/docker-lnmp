990613b7d9c241d3955b38827aae2e292e54d3b5
lbdong <lbdong2013@gmail.com>
2019-12-06 15:17:36 +0800

    logstash 规则

--- elk/kibana/entrypoint.sh
+++ elk/kibana/entrypoint.sh
@@ -2,4 +2,4 @@ curl -XPOST -D- 'http://kibana:5601/api/saved_objects/index-pattern' \
     -H 'Content-Type: application/json' \
     -H 'kbn-version: 7.4.2' \
     -u elastic:changeme \
-    -d '{"attributes":{"title":"nginx-access-*","timeFieldName":"@timestamp"}}'
+    -d '{"attributes":{"title":"llll-access-*","timeFieldName":"@timestamp"}}'
