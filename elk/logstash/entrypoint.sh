09646fce6adb0310ac15464be923ff95c3bdd057
lbdong <lbdong2013@gmail.com>
2019-12-06 17:31:14 +0800

    logstash 规则

--- elk/logstash/entrypoint.sh
+++ elk/logstash/entrypoint.sh
@@ -2,10 +2,21 @@
 
 set -e
 
-curl -XPOST -D- 'http://kibana:5601/api/saved_objects/index-pattern' \
+#创建kibana索引
+indexArr=("nginx-access-*" "nginx-error-*" "php-access-*" "php-error-*" "php-slow-*" "mysql-general-*")
+for indexName in ${indexArr[@]}
+do
+  until curl -XPOST -D- 'http://kibana:5601/api/saved_objects/index-pattern' \
     -H 'Content-Type: application/json' \
     -H 'kbn-version: 7.4.2' \
     -u elastic:changeme \
-    -d '{"attributes":{"title":"llll-access-*","timeFieldName":"@timestamp"}}'
+    -d '{"attributes":{"title":"'$indexName'-*","timeFieldName":"@timestamp"}}'
+  do
+      sleep 2
+      echo Retrying...
+  done
 
+done
+
+#启动logstash
 exec "logstash"
