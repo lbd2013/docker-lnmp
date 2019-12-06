dde524da4ecf1e8cb4484804ae3d26d677f0c302
lbdong <lbdong2013@gmail.com>
2019-12-06 17:49:30 +0800

    logstash 规则

--- elk/logstash/entrypoint.sh
+++ elk/logstash/entrypoint.sh
@@ -6,11 +6,14 @@ set -e
 indexArr=("nginx-access-*" "nginx-error-*" "php-access-*" "php-error-*" "php-slow-*" "mysql-general-*")
 for indexName in ${indexArr[@]}
 do
-  until curl -XPOST -D- 'http://kibana:5601/api/saved_objects/index-pattern' \
+  until $(curl -POST 'http://kibana:5601/api/saved_objects/index-pattern' \
     -H 'Content-Type: application/json' \
     -H 'kbn-version: 7.4.2' \
     -u elastic:changeme \
-    -d '{"attributes":{"title":"'$indexName'-*","timeFieldName":"@timestamp"}}'
+    -d '{"attributes":{"title":"'$indexName'-*","timeFieldName":"@timestamp"}}' \
+    -o /dev/null \
+    -s \
+    -w '%{http_code}') != "200"
   do
       sleep 2
       echo Retrying...
