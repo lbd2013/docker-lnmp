a8687bf223749f2a08c2ef9a93574818bc63ac53
lbdong <lbdong2013@gmail.com>
2019-12-10 15:39:25 +0800

    logstash 规则

--- elk/logstash/entrypoint.sh
+++ elk/logstash/entrypoint.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-#创建kibana索引
-indexArr=("nginx-access-*" "nginx-error-*" "php-access-*" "php-error-*" "php-slow-*" "mysql-general-*")
-for indexName in ${indexArr[@]}
-do
-  while true
-  do
-    tmpCode=$(curl -POST 'http://kibana:5601/api/saved_objects/index-pattern?overwrite=true' \
-      -H 'Content-Type: application/json' \
-      -H 'kbn-version: 7.4.2' \
-      -u elastic:changeme \
-      -d '{"attributes":{"title":"'$indexName'","timeFieldName":"@timestamp"}}' \
-      -o /dev/null \
-      -s \
-      -w '%{http_code}')
-    echo "http_code $tmpCode"
-
-    if [[ $tmpCode -eq 200 ]];then
-      echo "$indexName success"
-      break
-    else
-      sleep 2
-      echo "$indexName retrying..."
-    fi
-  done
-done
+##创建kibana索引
+#indexArr=("nginx-access-*" "nginx-error-*" "php-access-*" "php-error-*" "php-slow-*" "mysql-general-*")
+#for indexName in ${indexArr[@]}
+#do
+#  while true
+#  do
+#    tmpCode=$(curl -POST 'http://kibana:5601/api/saved_objects/index-pattern?overwrite=true' \
+#      -H 'Content-Type: application/json' \
+#      -H 'kbn-version: 7.4.2' \
+#      -u elastic:changeme \
+#      -d '{"attributes":{"title":"'$indexName'","timeFieldName":"@timestamp"}}' \
+#      -o /dev/null \
+#      -s \
+#      -w '%{http_code}')
+#    echo "http_code $tmpCode"
+#
+#    if [[ $tmpCode -eq 200 ]];then
+#      echo "$indexName success"
+#      break
+#    else
+#      sleep 2
+#      echo "$indexName retrying..."
+#    fi
+#  done
+#done
 
 set -e
 
