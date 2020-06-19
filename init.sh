aedfbc8f1999f81af9838ab3b2fefea2f2b11d30
Lin <lbdong2013@gmail.com>
2020-06-19 17:56:50 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ chmod -R 777 ./elk/logstash/pipeline
 chmod -R 777 ./zabbix/web/data
 chmod -R 777 ./zabbix/server/data
 chmod -R 777 ./grafana/data
+#docker-compose up -d
 docker-compose -f docker-compose.yml -f elk/docker-compose.yml up -d
